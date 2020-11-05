6342a91d0d409f8fdf9b9fd03bacc10f1c4a8660
golbian <florian13620@hotmail.fr>
2020-11-05 13:45:54 +0100

    ffff

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function reduireArray(array, size) {
 const dateTimeFormat = Intl.DateTimeFormat("fr");
 function afficher(json){
     let html = "";
-        selection.forEach(repo => {
+        json.forEach(repo => {
             console.log(repo)
             html += `<div class="column"><div class="card"><div class="card-image"><figure class="image is-4by3"><img src="${repo.link}"alt="Placeholder image"/></figure></div><div class="card-content"><div class="media"><div class="media-left"><figure class="image is-48x48"><img src="${repo.link}" alt="Placeholder image"/></figure></div><div class="media-content"><p class="title is-4">${repo.name}</p><p class="subtitle is-6">@Parcourir</p></div></div><div class="content">${repo.description}<br />Dernière mise à jour: <time datetime="${repo.datetime}">${dateTimeFormat.format(new Date(repo.datetime))}</time></div></div></div></div>`;
             html += "</div>";});
