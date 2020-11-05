ac463ccc7bf399f0eaa8e40e517cc589eecaec8a
golbian <florian13620@hotmail.fr>
2020-11-05 13:39:18 +0100

    test

--- script.js
+++ script.js
@@ -7,20 +7,24 @@ function reduireArray(array, size) {
 
 const dateTimeFormat = Intl.DateTimeFormat("fr");
 function afficher(json){
-    const repos = json.map(j => ({name: j.name,description: j.description || "",updated_at: j.updated_at}));
+    const repos = json.map(j => ({name: j.title,description: j.topic || "",link: j.link, datetime: j.datetime}));
+    console.log(json)
     const selections = reduireArray(repos, 3);
     let html = "";selections.forEach(selection => {
+        console.log(selection)
         html += '<div class="columns">';
         selection.forEach(repo => {
-            html += `<div class="column"><div class="card"><div class="card-image"><figure class="image is-4by3"><imgsrc="https://bulma.io/images/placeholders/1280x960.png"alt="Placeholder image"/></figure></div><div class="card-content"><div class="media"><div class="media-left"><figure class="image is-48x48"><imgsrc="https://bulma.io/images/placeholders/96x96.png"alt="Placeholder image"/></figure></div><div class="media-content"><p class="title is-4">${repo.name}</p><p class="subtitle is-6">@Parcourir</p></div></div><div class="content">${repo.description}<br />Dernière mise à jour: <time datetime="${repo.updated_at}">${dateTimeFormat.format(new Date(repo.updated_at))}</time></div></div></div></div>`;});
-            html += "</div>";});document.querySelector(".container").innerHTML = html;
+            console.log(repo)
+            html += `<div class="column"><div class="card"><div class="card-image"><figure class="image is-4by3"><img src="${repo.link}"alt="Placeholder image"/></figure></div><div class="card-content"><div class="media"><div class="media-left"><figure class="image is-48x48"><img src="${repo.link}" alt="Placeholder image"/></figure></div><div class="media-content"><p class="title is-4">${repo.name}</p><p class="subtitle is-6">@Parcourir</p></div></div><div class="content">${repo.description}<br />Dernière mise à jour: <time datetime="${repo.datetime}">${dateTimeFormat.format(new Date(repo.datetime))}</time></div></div></div></div>`;});
+            html += "</div>";});
+            document.querySelector(".container").innerHTML = html;
         }
 
         window.fetch("https://api.imgur.com/3/gallery/hot/viral/0.json").then(function(response) {
             return response.json();
           }).then(function(data) {
-              const json = data
-              document.addEventListener("DOMContentLoaded", function() {afficher(json)});
+              const json = data;
+              afficher(json);
             console.log(data);
           }).catch(function(err) {
             console.log(err);
