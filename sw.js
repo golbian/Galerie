c34e9da5a70ef5b021914dd96edfb681804e2577
golbian <florian13620@hotmail.fr>
2020-11-12 14:05:48 +0100

    sois bon stp

--- sw.js
+++ sw.js
@@ -1,10 +1,1 @@
-if('serviceWorker'in navigator) {
-    navigator.serviceWorker.register("/sw.js")
-    .then((reg) => {
-        console.log("votre service worker a été enregistré!");
-    })
-    .catch((error) => {
-        console.error(error);
-    })
-
 self.addEventListener('fetch',event=>{    event.RespondWith(newResponse('PWA!!!!'));});
