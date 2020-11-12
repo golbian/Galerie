af6fb76340babf2e4ac45ea2ea957b517ecfaf07
golbian <florian13620@hotmail.fr>
2020-11-12 14:01:31 +0100

    update register

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
+if('serviceWorker'in navigator) {
+    navigator.serviceWorker.getRegistrations().then(registrations=> {
+        for (let registration of registrations){
+            registrations.unregister();
+        }
+    }
+}
+
 self.addEventListener('fetch',event=>{    event.RespondWith(newResponse('PWA!!!!'));});
