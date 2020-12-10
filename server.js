71f4e20277bf88055b1e8bb44ff50e75580a29db
golbian <florian13620@hotmail.fr>
2020-12-10 10:38:20 +0100

    check68

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:8080"
 };
 app.use(cors(corsOptions));
 
