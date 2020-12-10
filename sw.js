454e0c27b20a9152fe79c8056bc58f6ffdfab859
golbian <florian13620@hotmail.fr>
2020-12-10 16:22:05 +0100

    check sw

--- sw.js
+++ sw.js
@@ -27,44 +27,44 @@ self.addEventListener("activate", e => {
 
 self.addEventListener("fetch", e => {console.log(e.request.url);});
 
-// self.addEventListener("fetch", event => {
-//   const url = event.request.url;
+self.addEventListener("fetch", event => {
+  const url = event.request.url;
 
-  // if (url.indexOf("https://lucid-bhabha-9f48ff.netlify.app/liste.json") === 0) {
-  //   event.respondWith(
-  //     fetch(event.request).then(response => {
-  //       if (response.status === 200) {
-	// 		console.info("Formatting data");
-	// 		return response.json().then(json => {
-	// 		const formattedResponse = json.map(j => ({
-	// 		name: j.name,
-	// 		description: j.description || "",
-	// 		updated_at: j.updated_at
-	// 		}));
+  if (url.indexOf("https://lucid-bhabha-9f48ff.netlify.app/liste.json") === 0) {
+    event.respondWith(
+      fetch(event.request).then(response => {
+        if (response.status === 200) {
+			console.info("Formatting data");
+			return response.json().then(json => {
+			const formattedResponse = json.map(j => ({
+			name: j.name,
+			description: j.description || "",
+			updated_at: j.updated_at
+			}));
 
-	// 		return new Response(JSON.stringify(formattedResponse));
-	// 		});
-  //       }
+			return new Response(JSON.stringify(formattedResponse));
+			});
+        }
 		
-	// 	else{
-	// 		console.error(
-	// 		"Service Worker",
-	// 		"Error when fetching",			
-	// 		event.request.url
-	// 		);
+		else{
+			console.error(
+			"Service Worker",
+			"Error when fetching",			
+			event.request.url
+			);
 
-	// 		return response;
-	// 	}
+			return response;
+		}
         
-  //     })
-  //   );
-  // } 
-  // else {
-  //   event.respondWith(
-  //     caches
-  //       .open(cacheName)
-  //       .then(cache => cache.match(event.request))
-  //       .then(response => response || fetch(event.request))
-  //   );
-  // }
-// });
+      })
+    );
+  } 
+  else {
+    event.respondWith(
+      caches
+        .open(cacheName)
+        .then(cache => cache.match(event.request))
+        .then(response => response || fetch(event.request))
+    );
+  }
+});
